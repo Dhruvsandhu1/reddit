957a65b2c633259d7ecbeb63f0123e8470783327
Dhruvsandhu1 <152141078+Dhruvsandhu1@users.noreply.github.com>
2024-10-19 01:37:05 +0530

    Update rsocial.py

--- rsocial.py
+++ rsocial.py
@@ -5,13 +5,16 @@ if __name__ == "__main__":
     st.title("Some relevant reviews from reddit on JAMF")
     st.markdown("______________________")
     with open("sentiment_insights.json", "r") as json_file:
-        file=json.load(json_file)
+        file = json.load(json_file)
         for elem in file:
-            if elem['insight']=='0':
+            if elem['useful'] == '0':  # Display reviews marked as '0'
                 if 'title' in elem:
                     st.markdown(elem['title'])
                 st.markdown(f"Written by {elem['author']}")
                 st.markdown(f"Subreddit : r/{elem['subreddit']}")
-                st.markdown(elem['body'])
+                st.markdown(f"Date : {elem['created'].split()[0]}")
+                st.markdown(f"Time : {elem['created'].split()[1]}")
+                with st.expander(f"{elem['insight']}"):
+                    st.markdown(elem['body'])
                 st.markdown(f"[Open URL]({elem['url']})")
                 st.markdown("______________________")
